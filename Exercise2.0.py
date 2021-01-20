165dabb5b8706cbfb41bc23b17d78b796ae8e8fb
nswit7 <77703586+nswit7@users.noreply.github.com>
2021-01-19 21:41:51 -0500

    Update Exercise2.0.py

--- Exercise2.0.py
+++ Exercise2.0.py
@@ -1,4 +1,4 @@
 print("Nicholas Switalski")
 print("1/19/2021")
-print("At what size of a project should you start using version management software? Why? Minimum 150 words.")
+print("At what size of a project should you start using version management software?")
 print("A version management software should be used on any size project. This is because if something happens while you're writing the code and lose progress there will always be a new version to restore to, and if you saved recently you won't lose as much progress.")
