6c3cc5b3764649a24bc8991a32ac80b85d86956d
nswit7 <77703586+nswit7@users.noreply.github.com>
2021-01-21 17:15:05 -0500

    Create Exercise2NicholasSwitalskiCIS104.py

---
+++ Exercise2NicholasSwitalskiCIS104.py
@@ -0,0 +1,15 @@
+print("Nicholas Switalski")
+
+print("1/19/2021")
+
+print("At what size of a project should you start using version management software? Why? Minimum 150 words.")
+
+print("A version management software should be used on any size project.")
+
+print("This is because if something happens while you're writing the code and lose progress there will always be a new version to restore to, and if you saved recently you won't lose as much progress.")
+
+print("Different versions also help because you can see your progress throughout the days of what you accomplished per day. Along with if you're in a group each member can see who has done what and when they did it.")
+
+print("Version Software also tracks every single change you make so if you're constantly checking if your code works and it's been working but you just added a new line and doesn't work you can know 100% that it's coming from that line you just made compared to your whole code saves time and energy.")
+
+print("Finally a version software should be used every single time you code simply because it can catch your mistakes quicker and faster if you're taking the correct steps to debugging or creating a code, without losing progress and having to restart.")
